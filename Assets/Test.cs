aa932439cfe673f74e227de3953cea828a8c8db1
Edvinas Skirgaila <edvinas.skirgaila@unity3d.com>
2022-06-15 12:43:37 +0300

    fix missing script tes.cs

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	private void OnEnable() {
+		var rend = GetComponent<MeshRenderer>();
+
+		var mpb = new MaterialPropertyBlock();
+		mpb.SetColor("_Color", new Color(Random.Range(0.8f, 1.0f), Random.Range(0.8f, 1.0f), Random.Range(0.8f, 1.0f), 1.0f));
+
+		rend.SetPropertyBlock(mpb);
+	}
+}
